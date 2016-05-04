507f8eb19019a68c3bbf7caa817a22d1eca25092
Jesse Moy <jmoy@wcs.org>
2016-05-04 13:00:32 -0400

    update logging

--- disturbance_script.py
+++ disturbance_script.py
@@ -6,15 +6,21 @@ import garden
 import arcpy
 import shutil
 import time
+from arcpy import env
+import pandas as pd
+import datetime
 
-# assign s.ecocommunities to starting raster
+# set environment
+
 arcpy.env.extent = s.ecocommunities
 arcpy.env.cellSize = s.ecocommunities
 arcpy.env.snapRaster = s.ecocommunities
 arcpy.env.outputCoordinateSystem = arcpy.Describe(s.ecocommunities).spatialReference
 arcpy.env.cartographicCoordinateSystem = arcpy.Describe(s.ecocommunities).spatialReference
-print arcpy.Describe(s.ecocommunities).spatialReference
-print arcpy.env.outputCoordinateSystem
+
+env.workspace = s.TEMP_DIR
+env.scratchWorkspace = s.TEMP_DIR
+env.overwriteOutput = True
 
 
 def clear_dir(directory):
@@ -31,37 +37,49 @@ clear_dir(os.path.join(s.INPUT_DIR, 'fire', 'script', 'burn_rasters'))
 
 clear_dir(s.TEMP_DIR)
 
+dist_type =['fire_area', 'fire_occurrence', 'pond_area', 'garden_area']
+disturbance_table = pd.DataFrame(columns=dist_type, index=s.RUN_LENGTH)
+
 s.logging.info('starting %s year simulation' % len(s.RUN_LENGTH))
 full_run_start = time.time()
-s.logging.info('start time: %s' % full_run_start)
+
+x = datetime.datetime
+
+s.logging.info('start time: %s' % x.now())
 for year in s.RUN_LENGTH:
 
     s.logging.info('____YEAR: %s____' % year)
 
     year_start = time.time()
-    s.logging.info('year start: %s' % year_start)
+    s.logging.info('start time: %s' % x.now())
 
     # garden
-    s.logging.info('starting garden disturbance')
+    s.logging.info('_____starting garden disturbance')
     garden_dis = garden.GardenDisturbance(year)
     garden_dis.run_year()
+    disturbance_table.loc[year]['garden_area'] = garden_dis.new_garden_area
 
     # fire
-    s.logging.info('starting fire disturbance')
+    s.logging.info('_____starting fire disturbance')
     fire_dis = fire.FireDisturbance(year)
     fire_dis.run_year()
+    disturbance_table.loc[year]['fire_occurrence'] = len(fire_dis.ignition_sites)
+    disturbance_table.loc[year]['fire_area'] = fire_dis.area_burned * s.CELL_SIZE
 
     # beaver pond
-    s.logging.info('starting pond disturbance')
+    s.logging.info('_____starting pond disturbance')
     pond_dis = pond.PondDisturbance(year)
     pond_dis.run_year()
-
+    disturbance_table.loc[year]['pond_area'] = pond_dis.new_pond_area
     year_end = time.time()
-    s.logging.info('year end: %s' % year_end)
-    s.logging.info('year run time: %s' % (year_end - year_start))
+    s.logging.info('end time time: %s' % x.now())
+    s.logging.info('year run time: %s minutes' % ((year_end - year_start)/60))
 
-s.logging.info('simulation completed')
+    disturbance_table.to_csv(path_or_buf=os.path.join(s.OUTPUT_DIR, 'disturbance_table.csv'))
+
+s.logging.info('______simulation completed')
 
 full_run_end = time.time()
-s.logging.info('end time: %s' % full_run_end)
-s.logging.info('full run time: %s' % (full_run_end - full_run_start))
+s.logging.info('end time: %s' % x.now)
+s.logging.info('full run time: %s minutes' % ((full_run_end - full_run_start)/60))
+
