61febd6dab7e64ad21c9b52939935f96e61574ab
Jesse Moy <jmoy@wcs.org>
2016-05-09 15:28:28 -0400

    settings toggle disturbances

--- disturbance_script.py
+++ disturbance_script.py
@@ -24,7 +24,6 @@ env.overwriteOutput = True
 
 
 def clear_dir(directory):
-
     file_list = os.listdir(directory)
     for file_name in file_list:
         path = (os.path.join(directory, file_name))
@@ -33,11 +32,12 @@ def clear_dir(directory):
         if os.path.isdir(path):
             shutil.rmtree(path)
 
+
 clear_dir(os.path.join(s.INPUT_DIR, 'fire', 'script', 'burn_rasters'))
 
 clear_dir(s.TEMP_DIR)
 
-dist_type =['fire_area', 'fire_occurrence', 'pond_area', 'garden_area']
+dist_type = ['fire_area', 'fire_occurrence', 'pond_area', 'garden_area']
 disturbance_table = pd.DataFrame(columns=dist_type, index=s.RUN_LENGTH)
 
 s.logging.info('starting %s year simulation' % len(s.RUN_LENGTH))
@@ -54,32 +54,37 @@ for year in s.RUN_LENGTH:
     s.logging.info('start time: %s' % x.now())
 
     # garden
-    s.logging.info('_____starting garden disturbance')
-    garden_dis = garden.GardenDisturbance(year)
-    garden_dis.run_year()
-    disturbance_table.loc[year]['garden_area'] = garden_dis.new_garden_area
+    if s.GARDEN:
+        s.logging.info('_____starting garden disturbance')
+        garden_dis = garden.GardenDisturbance(year)
+        garden_dis.run_year()
+        disturbance_table.loc[year]['garden_area'] = garden_dis.new_garden_area
 
     # fire
-    s.logging.info('_____starting fire disturbance')
-    fire_dis = fire.FireDisturbance(year)
-    fire_dis.run_year()
-    disturbance_table.loc[year]['fire_occurrence'] = len(fire_dis.ignition_sites)
-    disturbance_table.loc[year]['fire_area'] = fire_dis.area_burned * s.CELL_SIZE
+    if s.FIRE:
+        s.logging.info('_____starting fire disturbance')
+        fire_dis = fire.FireDisturbance(year)
+        fire_dis.run_year()
+        disturbance_table.loc[year]['fire_occurrence'] = len(fire_dis.ignition_sites)
+        disturbance_table.loc[year]['fire_area'] = fire_dis.area_burned * s.CELL_SIZE
 
     # beaver pond
-    s.logging.info('_____starting pond disturbance')
-    pond_dis = pond.PondDisturbance(year)
-    pond_dis.run_year()
-    disturbance_table.loc[year]['pond_area'] = pond_dis.new_pond_area
+    if s.POND:
+        s.logging.info('_____starting pond disturbance')
+        pond_dis = pond.PondDisturbance(year)
+        pond_dis.run_year()
+        disturbance_table.loc[year]['pond_area'] = pond_dis.new_pond_area
+
     year_end = time.time()
     s.logging.info('end time time: %s' % x.now())
-    s.logging.info('year run time: %s minutes' % ((year_end - year_start)/60))
+    s.logging.info('year run time: %s minutes' % ((year_end - year_start) / 60))
 
+    # track disturbances
     disturbance_table.to_csv(path_or_buf=os.path.join(s.OUTPUT_DIR, 'disturbance_table.csv'))
+    disturbance_table.to_csv(path_or_buf=os.path.join(s.LOG_DIR, 'disturbance_table.csv'))
 
 s.logging.info('______simulation completed')
 
 full_run_end = time.time()
 s.logging.info('end time: %s' % x.now)
-s.logging.info('full run time: %s minutes' % ((full_run_end - full_run_start)/60))
-
+s.logging.info('full run time: %s minutes' % ((full_run_end - full_run_start) / 60))
--- settings.py
+++ settings.py
@@ -10,6 +10,7 @@ ROOT_DIR = os.path.join('E:\\', '_data', 'welikia', 'WelikiaDisturbance')
 INPUT_DIR = os.path.join(ROOT_DIR, 'inputs')
 OUTPUT_DIR = os.path.join(ROOT_DIR, 'outputs')
 TEMP_DIR = os.path.join(ROOT_DIR, 'temp')
+LOG_DIR = os.path.join('C:\\', 'Users', 'LabGuest', 'Dropbox')
 
 ecocommunities = os.path.join(INPUT_DIR, 'ecocommunities.tif')
 
@@ -17,8 +18,13 @@ INPUT_FILES = [
     ecocommunities,
 ]
 
+# DISTURBANCE FLAG TOGGLE
+GARDEN = True
+FIRE = True
+POND = True
+
 # LOGGING
-logging.basicConfig(filename=os.path.join('C:\\', 'Users', 'LabGuest', 'Dropbox', 'disturbance_log.txt'),
+logging.basicConfig(filename=os.path.join(LOG_DIR, 'disturbance_log.txt'),
                     level=logging.DEBUG)
 
 # PARAMETERS
@@ -41,8 +47,8 @@ EXTINGUISH_THRESHOLD = 10
 CONDITIONING_LENGTH = 15
 
 # escaped fire probabilities
-EXPECTED_TRAIL_ESCAPE = 0.75
-EXPECTED_GARDEN_ESCAPE = 0.10
+EXPECTED_TRAIL_ESCAPE = 0.15
+EXPECTED_GARDEN_ESCAPE = .02
 PROB_HUNT_ESCAPE = 10
 
 # nonburnable fuel types
@@ -52,8 +58,11 @@ NONBURNABLE = [14, 16, 98, 99]
 TIME_TO_MID_FUEL = 20
 TIME_TO_CLIMAX_FUEL = 80
 
+# canopy based succession
+SHRUBLAND_CANOPY = 10
+
 # GUI controls
-INITIATE_RENDER_WAIT_TIME = 50
+INITIATE_RENDER_WAIT_TIME = 10
 SIMULATION_TIMEOUT = 5000
 
 # PONDS
@@ -63,6 +72,7 @@ CELL_SIZE = 5
 DAM_HEIGHT = 9
 
 # GARDENS
+PROXIMITY_BUFFER = 500
 PER_CAPITA_GARDEN_AREA = 1
 
 TIME_TO_ABANDON = 20
@@ -71,7 +81,7 @@ TIME_TO_ABANDON = 20
 
 # COMMUNITY CODES
 GARDEN_ID = 650  # ecosystem id for gardens (will look for this value when processing raster.
-OLD_FIELD_ID = 635  # 648  # ecosystem id for abandoned fields.
+GRASSLAND_ID = 635  # 648  # ecosystem id for abandoned fields.
 SHRUBLAND_ID = 649
 
 
@@ -97,11 +107,6 @@ class Disturbance(object):
 
 
 # Environment Setting
-env.workspace = TEMP_DIR
-print env.workspace
-env.scratchWorkspace = TEMP_DIR
-print env.scratchWorkspace
-env.overwriteOutput = True
 
 if arcpy.CheckExtension("Spatial") == "Available":
     arcpy.AddMessage("Checking out Spatial")
