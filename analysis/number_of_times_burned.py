14e605b8caae5bd22c07ce77ac4304ed48b5b200
Jesse Moy <jmoy@wcs.org>
2016-07-06 08:57:30 -0400

    refactor time since fire as a method

--- analysis/number_of_times_burned.py
+++ analysis/number_of_times_burned.py
@@ -7,17 +7,7 @@ from osgeo import gdal_array
 from prettyprint import *
 import glob
 import arcpy
-
-if arcpy.CheckExtension("Spatial") == "Available":
-    arcpy.AddMessage("Checking out Spatial")
-    arcpy.CheckOutExtension("Spatial")
-else:
-    arcpy.AddError("Unable to get spatial analyst extension")
-    arcpy.AddMessage(arcpy.GetMessages(0))
-    sys.exit(0)
-
-
-ROOT_DIR = r'E:\_data\welikia\disturbance_log\sensitivity_tests\extinguishing_threshold\200_year_threshold_50_trial_%s\burn_rasters'
+import settings as s
 
 def ascii_to_array(in_ascii_path):
     ascii = gdal.Open(in_ascii_path, GA_ReadOnly)
@@ -30,42 +20,44 @@ ll_corner = arcpy.Point(589779.34038235, 4515478.8842849)
 trials = range(1, 2)
 pp(trials)
 
-for trial in trials:
+def number_of_times_burned():
 
+    flame_dir =os.path.join(s.OUTPUT_DIR, 'fire', 'burn_rasters')
+    print flame_dir
+    raster_list = glob.glob1(os.path.join(s.OUTPUT_DIR, 'fire', 'burn_rasters'), '*_farsite_output.fml')
 
-    def number_of_times_burned():
+    pp(raster_list)
+    print len(raster_list)
 
-        raster_list = glob.glob1(ROOT_DIR % trial, '*_farsite_output.fml')
+    # # pp(raster_list)
+    times_burned = []
+    for i in raster_list:
+        print 'loading raster %s' % i
+        path = os.path.join(flame_dir, i)
 
-        pp(raster_list)
-        print len(raster_list)
+        a = ascii_to_array(path)
 
-        # # pp(raster_list)
-        times_burned = []
-        for i in raster_list:
+        # convert flame length map to binary burn/un-burn raster
+        a[a != -1] = 1
+        a[a == -1] = 0
 
-            path = os.path.join(ROOT_DIR % trial, i)
+        # print a[0]
 
-            a = ascii_to_array(path)
+        # append to trial list
+        times_burned.append(a)
 
-            # convert flame length map to binary burn/un-burn raster
-            a[a != -1] = 1
-            a[a == -1] = 0
+    print 'summing raster list'
+    t = sum(times_burned)
 
-            # print a[0]
+    print 'saving as tif'
+    out_ras = arcpy.NumPyArrayToRaster(t,
+                                       value_to_nodata=-1,
+                                       lower_left_corner=ll_corner,
+                                       x_cell_size=5)
 
-            # append to trial list
-            times_burned.append(a)
+    out_ras.save(os.path.join(s.OUTPUT_DIR, 'times_burned.tif'))
 
-        t = sum(times_burned)
-        out_ras = arcpy.NumPyArrayToRaster(t,
-                                           value_to_nodata=-1,
-                                           lower_left_corner=ll_corner,
-                                           x_cell_size=5)
-
-        out_ras.save(r'E:\_data\welikia\disturbance_log\sensitivity_tests\extinguishing_threshold\200_year_threshold_50_trial_%s\times_burned.tif' % trial)
-
-
+number_of_times_burned()
 
 
 
