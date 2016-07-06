8abdffb1459cdf9185df8570088d0ae999cd73f0
Jesse Moy <jmoy@wcs.org>
2016-07-06 08:56:41 -0400

    fill NaN values

--- analysis/ecosystem_areas.py
+++ analysis/ecosystem_areas.py
@@ -11,8 +11,8 @@ import datetime
 x = datetime.datetime
 
 print x.now()
-DIR = r"E:\_data\welikia\disturbance_log\sensitivity_tests\per-capita_garden_area\200_yr_trial_dependence_15\outputs"
-LOG_DIR = r"C:\Users\LabGuest\Dropbox\disturbance_logs\sensitivity_tests\per-capita_garden_area\200_yr_trial_dependence_15"
+# DIR = r"E:\_data\welikia\disturbance_log\sensitivity_tests\per-capita_garden_area\200_yr_trial_dependence_15\outputs"
+# LOG_DIR = r"C:\Users\LabGuest\Dropbox\disturbance_logs\sensitivity_tests\per-capita_garden_area\200_yr_trial_dependence_15"
 
 def raster_to_array(raster_path):
     raster = gdal.Open(raster_path, GA_ReadOnly)
@@ -26,16 +26,18 @@ def get_counts(array):
 
 def ecosystem_areas():
     communities = [600, 608, 609, 616, 621, 622, 625, 629, 632, 635, 644, 647, 649, 650, 654]
-    index = s.RUN_LENGTH
+    index = range(1409, 1610)  #s.RUN_LENGTH
     df = pd.DataFrame(columns=communities, index=index)
-    ecocommunities = os.path.join(DIR, 'ecocommunities_%s.tif')
+    ecocommunities = os.path.join(s.OUTPUT_DIR, 'ecocommunities_%s.tif')
 
-    for year in s.RUN_LENGTH:
+    for year in range(1409, 1610):
 
         if arcpy.Exists(ecocommunities % year):
             array = raster_to_array(ecocommunities % year)
             d = get_counts(array)
             df.loc[year] = pd.Series(d)
 
-    df.to_csv(path_or_buf=os.path.join(LOG_DIR, 'ecosystem_areas.csv'))
+    df.fillna(value=0, inplace=True)
+    df.to_csv(path_or_buf=os.path.join(s.LOG_DIR, 'ecosystem_areas.csv'))
 
+ecosystem_areas()
