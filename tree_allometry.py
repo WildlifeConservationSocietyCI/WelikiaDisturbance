c718e3b655c039b6c27f61f65f4a1d2c76751407
Jesse Moy <jmoy@wcs.org>
2016-09-02 10:21:35 -0400

    update tree allometry added tree_height_bean method

--- tree_allometry.py
+++ tree_allometry.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def tree_height(params, A, S):
+
+def tree_height_carmean(params, A, S):
     """
     tree height formula
     reference: 'Site Index Curves for Forest Tree Species in the Eastern United States'
@@ -25,6 +26,26 @@ def tree_height(params, A, S):
     H = b1 * S**b2 * (1 - np.exp(b3 * A)**(b4 * S**b5))
     return H
 
+
+def tree_height_bean(A):
+    """
+    logarithmic age-height relationship for red oak
+    reference: 'Using a spatially explicit ecological model to test scenarios
+    of fire use by Native Americans: An example from the Harlem Plains, New York, NY'
+    Bean and Sanderson 2007
+
+    H = tree height (ft)
+    A = age
+
+    :param A:
+    :return:
+    """
+
+    H = 44 * np.ma.log(A) - 93
+
+    return H
+
+
 def DBH_bean(A):
     """
     logarithmic age DBH relationship for red oak
@@ -39,8 +60,11 @@ def DBH_bean(A):
     :return:
     """
 
-    DBH = 25.706 * ( np.log(A) ) - 85.383
+    # ma.log takes log of values greater than 0
+    DBH = 25.706 * np.ma.log(A) - 85.383
+
     return DBH
+
 
 def DBH_eq_3_loewenstein(A):
     """
@@ -57,6 +81,7 @@ def DBH_eq_3_loewenstein(A):
 
     DBH = (A - 36.329) / 0.919
     return DBH
+
 
 def DBH_eq_2_loewenstein(A):
     """
@@ -75,13 +100,9 @@ def DBH_eq_2_loewenstein(A):
     return DBH
 
 
-shape = (3, 3)
-age = np.random.randint(0, 15, size=shape)
-
-print age
-
-p = {'b1': 6.1785,
-     'b2': 0.6619,
-     'b3': -0.0241,
-     'b4': 25.0185,
-     'b5': -0.74}
+# Site Index Curve Parameters
+red_oak_params = {'b1': 6.1785,
+                  'b2': 0.6619,
+                  'b3': -0.0241,
+                  'b4': 25.0185,
+                  'b5': -0.74}
