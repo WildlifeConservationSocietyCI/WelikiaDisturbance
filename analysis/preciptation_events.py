b5788024af68af2997f5373583ba0958303077c8
Jesse Moy <jmoy@wcs.org>
2017-08-21 11:41:13 -0400

    update precipitation events

--- analysis/preciptation_events.py
+++ analysis/preciptation_events.py
@@ -7,16 +7,17 @@ from prettyprint import *
 
 plt.style.use('ggplot')
 
-ROOT_DIR = r"E:\_data\welikia\WelikiaDisturbance\inputs\fire\wtr"
+ROOT_DIR = r"F:\_data\Welikia\WelikiaDisturbance"
+WTR_DIR = os.path.join(ROOT_DIR, 'inputs', 'fire', 'wtr')
 
-weather_files = glob.glob1(ROOT_DIR, "*.wtr")
+weather_files = glob.glob1(WTR_DIR, "*.wtr")
 
 # pp(weather_files)
 
 precip_events = []
 
 for file in weather_files:
-    path = os.path.join(ROOT_DIR, file)
+    path = os.path.join(WTR_DIR, file)
     # print path
     col_names = ['month', 'day', 'precip', 'hour1', 'hour2', 't1', 't2', 'h1', 'h2', 'elv']
     df = pd.read_csv(path,
@@ -28,24 +29,28 @@ for file in weather_files:
     # print df.head()
 
     slice = df.ix[df['month'] >= 3]
-    print slice.head()
+    print(slice.head())
     x = slice[slice['precip'] > 0]
     precip_events += x['precip'].tolist()
 
+
+
+
+
 # pp(precip_events)
 
-plt.hist(precip_events, bins=50, color='black')
+plt.hist(precip_events, bins=50, color='black', normed=True)
 plt.title('Precipitation Events 1876-2006')
 plt.ylabel('frequency')
 plt.xlabel('rainfall $1/100 in$')
 plt.xlim(0, 850)
 pe = pd.Series(precip_events, name='precip')
 
-# summary = pe.describe()
+summary = pe.describe()
 # print summary.values
 # print summary.index
-# # summary = pd.DataFrame(data=[int(pe.mean), int(pe.std), int(pe.argmin), int(pe.argmax)],
-# #                        index=['mean', 'std', 'min', 'max'])
+# summary = pd.DataFrame(data=[int(pe.mean), int(pe.std), int(pe.argmin), int(pe.argmax)],
+#                        index=['mean', 'std', 'min', 'max'])
 # plt.table(cellText=summary.values,
 #           rowLabels=summary.index,
 #           cellLoc='center',
@@ -53,6 +58,10 @@ pe = pd.Series(precip_events, name='precip')
 #           loc='top')
 
 
-plt.show()
+# plt.show()
 
-print pe.describe()
+# print(pe.describe())
+
+precip = df['precip']
+print(precip.head())
+print(df[df['precip'] > 10].count / df.count())
