71f63614b49760a254d01a3410da697ff83e57f6
LabGuest <LabGuest>
2017-08-21 16:36:13 -0400

    settings_local template

---
+++ settings_local_template.py
@@ -0,0 +1,13 @@
+### COPY THIS FILE TO settings_local.py ###
+### THEN POPULATE BELOW BASED ON LOCAL ENVIRONMENT ###
+
+import os
+
+# DIRECTORIES
+TRIAL_NAME = ''  # 'region_4_test'
+
+ROOT_DIR = ''  # os.path.join(r'D:\_data\Welikia\WelikiaDisturbance')
+REGION = ''  # '4'
+LOG_DIR = ''  # r'D:\_data\Welikia\disturbance_logs\%s' % TRIAL_NAME
+FARSITE = ''  # r'C:\Program Files (x86)\FARSITE4\farsite4.exe'
+TEMP_DIR = ''  # r'D:\_data\Welikia\disturbance_logs\region_4_temp'
