5540b0e2687253ea654ca9461bbab9c13cda360d
Jesse Moy <jmoy@wcs.org>
2017-08-21 15:07:35 -0400

    add local settings template

---
+++ settings_local.py
@@ -0,0 +1,10 @@
+import os
+
+# DIRECTORIES
+TRIAL_NAME = 'fuel_dbh_test'
+
+ROOT_DIR = os.path.join(r'F:\_data\Welikia\WelikiaDisturbance')
+REGION = '4'
+LOG_DIR = r'F:\Dropbox\disturbance_logs\%s' % TRIAL_NAME
+FARSITE = r'C:\Program Files (x86)\FARSITE4\farsite4.exe'
+TEMP_DIR = r'F:\_data\Welikia\WelikiaDisturbance\tests\scratch_workspace.gdb'
